222fb5e9aef5d70e99a3dfc323197ed99edd692c
abdel3107 <abdelzidane3107@gmail.com>
2024-10-27 16:48:56 +0100

    Initial commit

---
+++ src/main/java/org/example/ongere/OngereSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.ongere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngereSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngereSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ongere/OngereSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ongere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngereSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
