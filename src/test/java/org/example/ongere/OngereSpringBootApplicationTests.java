254ca4ef6975075221a10fe41455c36bd3f4f017
abdel3107 <abdelzidane3107@gmail.com>
2024-10-28 16:50:15 +0100

    Project setup

--- src/test/java/org/example/ongere/OngereSpringBootApplicationTests.java
+++ src/test/java/org/example/ongere/OngereSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OngereSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
